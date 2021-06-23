5b2d858f6444d56b694f694f7552491679aee942
Josiah Davis <josiah.j.davis@gmail.com>
2021-06-23 16:51:28 +0800

    Create argparsing.py

---
+++ argparsing.py
@@ -0,0 +1,27 @@
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="My description")
+    
+    # Setting a parameter
+    parser.add_argument("--region", default="ap-southeast-1", help="aws region")
+    
+    # Specifying the type
+    parser.add_argument("-p", "--my-param", default=5, type=int, help="Param description")
+    
+    # Setting options
+    parser.add_argument("--model", default="ppo", choices=["ppo", "es", "ars"], help="My learning algorithm")
+    
+    # Bool
+    parser.add_argument(
+        "--mybool",
+        dest="mybool",
+        action="store_true",
+        help="Whether to use this or not",
+    )
+    parser.add_argument("--no-mybool", dest="mybool", action="store_false")
+    parser.set_defaults(mybool=False)
+    
+    # Parsing the arguments
+    args = parser.parse_args()
+    print(args)
